a3dcdf754d0b487fe0b5d07d177a77e5fa863c43
XavierPiboule <piboule.xavier@gmail.com>
2024-12-16 00:22:03 +0100

    Final

--- Cards_To_The_Eye/form.js
+++ Cards_To_The_Eye/form.js
@@ -1,48 +1,57 @@
-let messageS = document.querySelector("message-success");
-let messageE1 = document.querySelector("error-name");
-let messageE2 = document.querySelector("error-password");
+let messageS = document.querySelector(".message-success");
+let messageE1 = document.querySelector(".error-name");
+let messageE2 = document.querySelector(".error-password");
 let form = document.querySelector("form");
 let pseudo = document.querySelector("#pseudo");
 let passWord = document.querySelector("#password");
 let passWord2 = document.querySelector("#password2");
-//vérifiacation minuscule, majuscule, nombre et caractère spéciaux
-let passCheck = new RegExp(
-    "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*., ?]).+$");
 
-console.log(form);
-console.log(pseudo);
+//Vérification des conditions pour le mot de passe (majuscule, minuscule, chiffre, caractère spécial)
+let passCheck = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*., ?]).+$");
 
-messageE1.classList.add("message-appear");
+//les messages sont masqués par défaut
+messageS.style.display = "none";
+messageE1.style.display = "none";
+messageE2.style.display = "none";
 
 form.addEventListener("submit", function(event){
     let valide = true
+
+    messageS.style.display = "none";
+    messageE1.style.display = "none";
+    messageE2.style.display = "none";
 
     event.preventDefault();
     //vérification pseudo
     if (pseudo.value.length < 6) { 
         console.log("error pseudo");
+        messageE1.style.display = "block";
         valide = false;
     }
     else{
         console.log("pseudo good");
-            //vérification mot de passe
-        if (passWord.value.length < 10 || passCheck.test(passWord.value) == false) { 
-            console.log("error mdp");
-            valide = false;
+        messageE2.style.display = "none";
         }
-        else{
-            console.log("mdp good");
-                //vérification des mots de passe
-                if(passWord.value !== passWord2.value){
-                    console.log("mdp pas identique");
-                    valide = false;
-                }
-                else{
-                    console.log("mdp identique");
-                }
-        }
+
+    if(passWord.value !== passWord2.value){
+        console.log("mdp pas identique");
+        valide = false;
+        messageE2.style.display = "block"
+    }
+    else{
+        console.log("mdp identique");
+        messageE2.style.display = "none"
+    }
+    if (passWord.value.length < 10 || passCheck.test(passWord.value) == false) { 
+        console.log("error mdp");
+        valide = false;
+        messageE2.style.display = "block";
+    }
+    else{
+        messageE2.style.display = "none";
+
     }
     if(valide == true){
-        
+        messageS.style.display = "block";
     }
 });
